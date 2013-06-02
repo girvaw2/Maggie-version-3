e05f1a864794a842dbe474c485a3abf9f709e0a5
Billy <girvaw@gmail.com>
2013-06-02 20:58:20 +0100

    Further ball tracking stuff

--- tools/arm_tests/arm_test_gui/include/arm_test_gui/trackball.h
+++ tools/arm_tests/arm_test_gui/include/arm_test_gui/trackball.h
@@ -8,7 +8,7 @@
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 #include <boost/asio/io_service.hpp>
-//#include <QtGui>
+#include <boost/format.hpp>
 
 using namespace cv;
 
@@ -16,35 +16,38 @@ class TrackBall
 {
 public:
     TrackBall(int hueLower, int hueUpper, int saturationLower, int saturationUpper, int valueLower, int valueUpper);
-     void doSomethingOp() const ;
+    void doSomethingOp() const ;
 
-     void setHueLowerValue(int value);
-     void setHueUpperValue(int value);
-     void setSaturationLowerValue(int value);
-     void setSaturationUpperValue(int value);
-     void setValueLowerValue(int value);
-     void setValueUpperValue(int value);
-     void loop();
+    void setHueLowerValue(int value);
+    void setHueUpperValue(int value);
+    void setSaturationLowerValue(int value);
+    void setSaturationUpperValue(int value);
+    void setValueLowerValue(int value);
+    void setValueUpperValue(int value);
+    void loop();
 
 private:
     void image_cb (const sensor_msgs::Image& rgbImage);
-    void applyHSVRange(cv_bridge::CvImage &cv_ptr);
-    void houghCircle(Mat &frame_gray, vector<Vec3f> &circles);
+    void depth_cb (const sensor_msgs::Image& depthImage);
+    void drawCircles(cv_bridge::CvImage &cv_ptr);
+    void getHoughCircle(Mat &frame_gray, vector<Vec3f> &circles);
     void trackObject(cv_bridge::CvImage &cv_ptr);
     ros::NodeHandle *getNodeHandle();
 
 private:
     ros::NodeHandle *nodeHandle;
-    int hueLower;
-    int hueUpper;
-    int saturationLower;
-    int saturationUpper;
-    int valueLower;
-    int valueUpper;
+    int hueLower_;
+    int hueUpper_;
+    int saturationLower_;
+    int saturationUpper_;
+    int valueLower_;
+    int valueUpper_;
+    Point center_;
 
-    ros::Subscriber image_sub_; //image subscriber
-    ros::Publisher image_pub_; //image message publisher
-    ros::Publisher image_hsv_range_pub_; //hsv range image message publisher
+    ros::Subscriber image_sub_;
+    ros::Subscriber depth_sub_;
+    ros::Publisher image_pub_;
+    ros::Publisher image_hsv_range_pub_;
 
     boost::asio::io_service service;
 };
--- tools/arm_tests/arm_test_gui/src/trackball.cpp
+++ tools/arm_tests/arm_test_gui/src/trackball.cpp
@@ -1,34 +1,32 @@
 #include "arm_test_gui/trackball.h"
 
 TrackBall::TrackBall(int hueLower, int hueUpper, int saturationLower, int saturationUpper, int valueLower, int valueUpper) :
-    hueLower(hueLower),
-    hueUpper(hueUpper),
-    saturationLower(saturationLower),
-    saturationUpper(saturationUpper),
-    valueLower(valueLower),
-    valueUpper(valueUpper)
+    hueLower_(hueLower),
+    hueUpper_(hueUpper),
+    saturationLower_(saturationLower),
+    saturationUpper_(saturationUpper),
+    valueLower_(valueLower),
+    valueUpper_(valueUpper)
 {
     nodeHandle = (ros::NodeHandle *)0;
 }
 
-void TrackBall::setHueLowerValue(int value) { hueLower = value; }
-void TrackBall::setHueUpperValue(int value) { hueUpper = value; }
-void TrackBall::setSaturationLowerValue(int value) { saturationLower = value; }
-void TrackBall::setSaturationUpperValue(int value) { saturationUpper = value; }
-void TrackBall::setValueLowerValue(int value) { valueLower = value; }
-void TrackBall::setValueUpperValue(int value) { valueUpper = value; }
+void TrackBall::setHueLowerValue(int value) { hueLower_ = value; }
+void TrackBall::setHueUpperValue(int value) { hueUpper_ = value; }
+void TrackBall::setSaturationLowerValue(int value) { saturationLower_ = value; }
+void TrackBall::setSaturationUpperValue(int value) { saturationUpper_ = value; }
+void TrackBall::setValueLowerValue(int value) { valueLower_ = value; }
+void TrackBall::setValueUpperValue(int value) { valueUpper_ = value; }
 
 void
 TrackBall::image_cb (const sensor_msgs::Image& rgbImage)
 {
-    std::cout << "image_cb " << hueUpper << " " << saturationUpper << " " << valueUpper << std::endl;
-
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
         cv_ptr = cv_bridge::toCvCopy(rgbImage, sensor_msgs::image_encodings::BGR8);
 
-        applyHSVRange(*cv_ptr);
+        drawCircles(*cv_ptr);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -37,13 +35,34 @@ TrackBall::image_cb (const sensor_msgs::Image& rgbImage)
     }
 }
 
-void TrackBall::applyHSVRange(cv_bridge::CvImage &cv_ptr)
+void
+TrackBall::depth_cb (const sensor_msgs::Image& depthImage)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(depthImage, sensor_msgs::image_encodings::TYPE_32FC1);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    float z = cv_ptr->image.at<float>(center_);
+    if (std::isnan<float>(z) == false)
+    {
+           std::cout << boost::format("depth_cb %1%") %  z << std::endl;
+    }
+}
+
+void TrackBall::drawCircles(cv_bridge::CvImage &cv_ptr)
 {
     Mat frame_hsv;
     Mat frame_hsv_range;
     cvtColor( cv_ptr.image, frame_hsv, CV_BGR2HSV );
 
-    inRange(frame_hsv, cv::Scalar(hueLower,saturationLower,valueLower), cv::Scalar(hueUpper,saturationUpper, valueUpper), frame_hsv_range);
+    inRange(frame_hsv, cv::Scalar(hueLower_,saturationLower_,valueLower_), cv::Scalar(hueUpper_,saturationUpper_, valueUpper_), frame_hsv_range);
 
     cv_bridge::CvImage cv_ptr2;
 
@@ -53,23 +72,26 @@ void TrackBall::applyHSVRange(cv_bridge::CvImage &cv_ptr)
     image_hsv_range_pub_.publish(cv_ptr2.toImageMsg());
 
     vector<Vec3f> circles;
-    houghCircle(frame_hsv_range, circles); // remember, frame_hsv_range is already in grayscale!
+    getHoughCircle(frame_hsv_range, circles); // remember, frame_hsv_range is already in grayscale!
 
     /// Draw the circles detected
     for( size_t i = 0; i < circles.size(); i++ )
     {
-        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        center_.x = cvRound(circles[i][0]);
+        center_.y = cvRound(circles[i][1]);
+        //center_(cvRound(circles[i][0]), cvRound(circles[i][1]));
         int radius = cvRound(circles[i][2]);
         // circle center
-        circle( cv_ptr.image, center, 3, Scalar(0,255,0), -1, 8, 0 );
+        circle( cv_ptr.image, center_, 3, Scalar(0,255,0), -1, 8, 0 );
         // circle outline
-        circle( cv_ptr.image, center, radius, Scalar(0,0,255), 3, 8, 0 );
+        circle( cv_ptr.image, center_, radius, Scalar(0,0,255), 3, 8, 0 );
     }
+
     if (circles.size())
         image_pub_.publish(cv_ptr.toImageMsg());
 }
 
-void TrackBall::houghCircle(Mat &frame_gray, vector<Vec3f> &circles)
+void TrackBall::getHoughCircle(Mat &frame_gray, vector<Vec3f> &circles)
 {
     GaussianBlur( frame_gray, frame_gray, Size(9, 9), 2, 2 );
 
@@ -79,55 +101,7 @@ void TrackBall::houghCircle(Mat &frame_gray, vector<Vec3f> &circles)
 
 void TrackBall::trackObject(cv_bridge::CvImage &cv_ptr)
 {
-    //cv::Moments moments()
 
-    Mat canny_output;
-    int thresh = 100;
-    int max_thresh = 255;
-    cv::Canny(cv_ptr.image, canny_output, thresh, thresh*2, 3 );
-
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-    /// Get the moments
-    vector<Moments> mu(contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-    {
-        mu[i] = moments( contours[i], false );
-    }
-
-    ///  Get the mass centers:
-    vector<Point2f> mc( contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-    {
-        mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-    }
-
-
-
-    RNG rng(12345);
-    /// Draw contours
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-        circle( drawing, mc[i], 4, color, -1, 8, 0 );
-    }
-
-    for( int i = 0; i< contours.size(); i++ )
-       {
-         printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
-         Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-         drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-         circle( drawing, mc[i], 4, color, -1, 8, 0 );
-       }
-
-    cv_ptr.image = drawing;
-    cv_ptr.encoding = sensor_msgs::image_encodings::BGR8;
-    image_pub_.publish(cv_ptr.toImageMsg());
 }
 
 
@@ -137,10 +111,17 @@ void TrackBall::loop()
 
     std::string image_topic = "/camera/rgb/image_color";
     image_sub_ = getNodeHandle()->subscribe (image_topic, 1, &TrackBall::image_cb, this);
+
+    std::string depth_topic = "/camera/depth_registered/image";
+    depth_sub_ = getNodeHandle()->subscribe (depth_topic, 1, &TrackBall::depth_cb, this);
+
     image_pub_ = getNodeHandle()->advertise<sensor_msgs::Image> ("ball_image", 30);
     image_hsv_range_pub_ = getNodeHandle()->advertise<sensor_msgs::Image> ("hsv_range_image", 30);
 
     std::string r_it = getNodeHandle()->resolveName (image_topic);
+    ROS_INFO_STREAM("Listening for incoming data on topic " << r_it );
+
+    r_it = getNodeHandle()->resolveName (depth_topic);
     ROS_INFO_STREAM("Listening for incoming data on topic " << r_it );
 
     ros::Rate r(10);
